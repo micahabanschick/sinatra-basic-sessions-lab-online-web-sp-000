7f5bce6f15f650c841414f2d92afd7f14f297937
Micah Banschick <micha.ban@gmail.com>
2020-08-21 21:06:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions
+    set :session_secret, "this is the elaborately long secret"
+  end
+  
+  get '/' do 
+    erb :index
+  end 
+  
+  post '/checkout' do 
+    @session = params[:item]
+    # binding.pry
+    erb :checkout
+  end 
+  
 end

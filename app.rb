2eb1a8f843ea470a7c609bca71194c14c2f74d43
Micah Banschick <micha.ban@gmail.com>
2020-08-21 21:21:32 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions
+    set :session_secret, "this is the elaborately long secret"
+  end
+  
+  get '/' do 
+    erb :index
+  end 
+  
+  post '/checkout' do 
+    @item = params[:item]
+    session[:item] = @item
+    # binding.pry
+    erb :checkout
+  end 
+  
 end
